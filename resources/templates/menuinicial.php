d951d6a19c9fb78b81d965ef4668704bf6ec57c3
ruialmeida96 <34514089+ruialmeida96@users.noreply.github.com>
2018-04-01 03:50:48 +0100

    Testes

--- resources/templates/menuinicial.php
+++ resources/templates/menuinicial.php
@@ -36,7 +36,7 @@
 
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link" href="#">
+            <a class="nav-link" href="#exampleModal" data-toggle="modal">
               <i class="material-icons">person</i>
               <p>
                 <span class="d-lg-none d-md-block">Account</span>
@@ -51,6 +51,27 @@
 
   <div class="content">
     <div class="container-fluid">
+
+      <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Efetuar Login</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              Email
+              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-success">Entrar</button>
+            </div>
+          </div>
+        </div>
+      </div>
 
 
       <?php
